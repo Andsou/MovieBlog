82378abbd566d9020f51480982f8a4cf24679a3b
andsou <andysoutcharith@gmail.com>
2022-03-23 18:43:52 -0500

    Added a bunch of posts to the home page and allowing users to edit or view the post

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     * Description: Home page with all blog posts
     */
 
-   require('dbConnect.php');
+   require('DbConnect.php');
 
-   $query = "SELECT * FROM posts LIMIT 5";
+   $query = "SELECT * FROM movies LIMIT 10";
 
    $statement = $db->prepare($query);
 
@@ -19,24 +19,33 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="styles.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="styles.css">
         <title>Home</title>
     </head>
     <body>
-        <div class="home">
-            <div id="header">
-                <a class="title" href="index.php"><h1>Andy's Movie Blog</h1></a>
-                <a id="new-post" href="create.php"><button>New Post</button></a>
-            </div>
-            <?php while($row = $statement->fetch()): ?>
-                <h2><a class="title" href="description.php?id=<?= $row['id'] ?>"><?= $row['title'] ?></a></h2>
-                <small><?= date("F j, Y, g:i a", strtotime($row['date'])) ?><a href="edit.php?id=<?= $row['id'] ?>">Edit post</a></small>
-                <?php if (strlen($row['content']) > 200): ?>                   
-                    <p><?= substr_replace($row['content'],"<a href=description.php?id=" . $row['id'] . ">Read more...</a>", 200);?></p>
-                <?php else:?>
-                    <p><?= $row['content'] ?></p>
-                <?php endif ?>
-            <?php endwhile ?> 
+        <div id="header">
+            <ul id="nav-buttons">
+                <li><a class="text-decoration-none" href="index.php"><h1>Andy's Movie Blog</h1></a></li>
+
+                <div class="post-buttons">
+                <li><a id="new-post" href="create.php">New Post</a></li>
+                <li><a id="home-page" href="index.php">Home</a></li>
+                </div>
+            </ul>                         
+        </div> 
+
+        <div class="container">
+        <?php while($row = $statement->fetch()): ?>
+            <h2><a class="text-decoration-none" href="description.php?movieId=<?= $row['movieId'] ?>"><?= $row['movieName'] ?></a></h2>
+            <h4><?= "Released: " . $row['releaseDate'] ?><button type="button" class="btn btn-light"><a class="text-decoration-none" href="update.php?movieId=<?= $row['movieId'] ?>">Edit post</a></button></h4>
+            <small><?= "Posted on: " . date("F j, Y, g:i a", strtotime($row['postTime'])) ?></small>
+            <?php if (strlen($row['description']) > 500): ?>                   
+                <p><?= substr_replace($row['description'],"<a href=description.php?movieId=" . $row['movieId'] . ">Read more...</a>", 500);?></p>
+            <?php else:?>
+                <p><?= $row['description'] ?></p>
+            <?php endif ?>
+        <?php endwhile ?>
         </div>
     </body>
 </html>
