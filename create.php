82c8d00bd13905bc2a569c02909a78333e5ea44d
andsou <andysoutcharith@gmail.com>
2022-03-23 18:47:36 -0500

    Added bootstrap css to the create page

--- create.php
+++ create.php
@@ -42,35 +42,48 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="styles.css">
         <title>Create new post</title>
     </head>
     <body>
-        <div class="home">
-            <a class="title" href="index.php"><h1>Andy's Movie Blog</h1></a>
-            <form method="post" action="create.php">
-                <label for="movieName">Movie's Name</label>
-                <input id="movieName" name="movieName">
+        <div id="header">
+            <ul id="nav-buttons">
+                <li><a class="text-decoration-none" href="index.php"><h1>Andy's Movie Blog</h1></a></li>
 
-                <label for="directorName">Director's Name</label>
-                <input id="directorName" name="directorName">
-
-                <label for="movieGenre">Movie's Genre(s)</label>
-                <input id="movieGenre" name="movieGenre">
-
-                <label for="releaseDate">Release Date</label>
-                <input id="releaseDate" name="releaseDate">
-                
-                <div id="text-area">
-                    <label for="description">Description</label>
-                    <textarea id="description" name="description" rows="10" cols="50"></textarea>
+                <div class="post-buttons">
+                <li><a id="new-post" href="create.php">New Post</a></li>
+                <li><a id="home-page" href="index.php">Home</a></li>
                 </div>
-                <div id="text-area">
-                    <label for="notes">Notes</label>
-                    <textarea id="notes" name="notes" rows="10" cols="50"></textarea>
-                    <input type="submit" value="Submit Post">
-                </div>
-            </form>
+            </ul>                         
+        </div> 
+
+        <div class="container">
+            <div class="form-group">
+                <form method="post" action="create.php">
+                    <label for="movieName" placeholder="Enter the movie's name">Movie's Name</label>
+                    <input id="movieName" type="text" name="movieName" class="form-control" placeholder="Enter the movie's name">
+
+                    <label for="directorName">Director's Name</label>
+                    <input id="directorName" type="text" name="directorName" class="form-control">
+
+                    <label for="movieGenre">Movie's Genre(s)</label>
+                    <input id="movieGenre" type="text" name="movieGenre" class="form-control">
+
+                    <label for="releaseDate">Release Date</label>
+                    <input id="releaseDate" name="releaseDate" class="form-control">
+                    
+                    <div id="text-area">
+                        <label for="description">Description</label>
+                        <textarea id="description" name="description" rows="10" cols="50"></textarea>
+                    </div>
+                    <div id="text-area">
+                        <label for="notes">Notes</label>
+                        <textarea id="notes" name="notes" rows="10" cols="50"></textarea>
+                        <input type="submit" value="Submit Post">
+                    </div>
+                </form>
+            </div>
         </div>
     </body>
 </html>
