fc5776d2d77c08cf3b61bbf16da22f9859db63c5
andsou <andysoutcharith@gmail.com>
2022-03-23 18:44:32 -0500

    Users can see the full post from the home page

--- description.php
+++ description.php
@@ -6,30 +6,42 @@
     * Description: View a post
     */
 
-    require('dbConnect.php');
+    require('DbConnect.php');
         
-    $query = "SELECT * FROM movies WHERE movieId = :MovieId LIMIT 1";
+    $query = "SELECT * FROM movies WHERE movieId = :movieId LIMIT 1";
     $statement = $db->prepare($query);
 
-    $movieId = filter_input(INPUT_GET, 'movieId', FILTER_SANITIZE_NUMBER_INT);
+    $id = filter_input(INPUT_GET, 'movieId', FILTER_SANITIZE_NUMBER_INT);
 
-    $statement->bindValue('movieId', $movieId, PDO::PARAM_INT);
+    $statement->bindValue('movieId', $id, PDO::PARAM_INT);
     $statement->execute();
 
     $row = $statement->fetch();
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="styles.css">
         <title>Full post</title>
     </head>
+    <div id="header">
+            <ul id="nav-buttons">
+                <li><a class="text-decoration-none" href="index.php"><h1>Andy's Movie Blog</h1></a></li>
+
+                <div class="post-buttons">
+                <li><a id="new-post" href="create.php">New Post</a></li>
+                <li><a id="home-page" href="index.php">Home</a></li>
+                </div>
+            </ul>                         
+        </div> 
     <body>
-        <div class="home">
+        <div class="container">
         <a class="title" href="index.php"><h1><?= $row['movieName'] ?></h1></a>
-            <small><?= date("F j, Y", strtotime($row['postTime'])) ?><a href="update.php?id=<?= $row['movieId'] ?>">Edit post</a></small>
+            <small><?= date("F j, Y", strtotime($row['postTime'])) ?><a href="edit.php?movieId=<?= $row['movieId'] ?>">Edit post</a></small>
             <p><?= $row['description'] ?></p>
         </div>
     </body>
