1d7902fd99ad92a252fcf4b43d3fc1b34615ea2c
andsou <andysoutcharith@gmail.com>
2022-03-23 04:36:20 -0500

    Added an index file that specifies the home page

--- index.php
+++ index.php
@@ -1,17 +1,42 @@
 <?php
-/*
- * Name: Andy Soutcharith
- * Program: Business Information Technology
- * Course: ADEV-1008 Programming
- * Created: 2021/04/13
- * Updated: 2021/04/13
- */
+   /**
+    * Home page
+    * Name: Andy Soutcharith
+    * Date: March 23, 2022
+    * Description: Home page with all blog posts
+    */
 
-    require('dbConnect.php');
+   require('dbConnect.php');
 
-    $query = "SELECT * FROM posts LIMIT 5";
+   $query = "SELECT * FROM posts LIMIT 5";
 
-    $statement = $db->prepare($query);
+   $statement = $db->prepare($query);
 
-    $statement->execute();
- ?>
+   $statement->execute();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="styles.css">
+        <title>Home</title>
+    </head>
+    <body>
+        <div class="home">
+            <div id="header">
+                <a class="title" href="index.php"><h1>Andy's Movie Blog</h1></a>
+                <a id="new-post" href="create.php"><button>New Post</button></a>
+            </div>
+            <?php while($row = $statement->fetch()): ?>
+                <h2><a class="title" href="description.php?id=<?= $row['id'] ?>"><?= $row['title'] ?></a></h2>
+                <small><?= date("F j, Y, g:i a", strtotime($row['date'])) ?><a href="edit.php?id=<?= $row['id'] ?>">Edit post</a></small>
+                <?php if (strlen($row['content']) > 200): ?>                   
+                    <p><?= substr_replace($row['content'],"<a href=description.php?id=" . $row['id'] . ">Read more...</a>", 200);?></p>
+                <?php else:?>
+                    <p><?= $row['content'] ?></p>
+                <?php endif ?>
+            <?php endwhile ?> 
+        </div>
+    </body>
+</html>
