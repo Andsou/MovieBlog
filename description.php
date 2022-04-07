fded473b457469632503297f8aafbe2b97a35dba
andsou <andysoutcharith@gmail.com>
2022-04-07 01:38:09 -0500

    Added image resizing with php when images are uploaded to the website

--- description.php
+++ description.php
@@ -8,6 +8,7 @@
 
     require('DbConnect.php');
 
+    // Join all 3 tables in the database together
     $query = "SELECT * FROM movies 
               LEFT JOIN comments on movies.movieId = comments.movieIdFk
               LEFT JOIN images on images.movieIds = movies.movieId 
@@ -39,25 +40,39 @@
         }        
     }
 
+    // Handles the uploading of the image
     if (isset($_POST['upload'])) {
         $target = "images/" . basename($_FILES['image']['name']);
         $image = $_FILES['image']['name'];
         $movieId = filter_input(INPUT_POST, 'movieId', FILTER_SANITIZE_NUMBER_INT);
         
+        $result = move_uploaded_file($_FILES['tmp_name'], $target);
+        $orig_image = imagecreatefromjpeg($target);
+        $image_info = getimagesize($target); 
+        $width_orig  = $image_info[0]; // current width as found in image file
+        $height_orig = $image_info[1]; // current height as found in image file
+        $width = 1024; // new image width
+        $height = 768; // new image height
+        $destination_image = imagecreatetruecolor($width, $height);
+        imagecopyresampled($destination_image, $orig_image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
+        // This will just copy the new image over the original at the same filePath.
+        imagejpeg($destination_image, $target, 100);
+
         $query = "INSERT INTO images (image, movieIds) VALUES ('$image', :movieIds)"; 
 
-        move_uploaded_file($_FILES['image']['tmp_name'], $target);
+
 
         $statement = $db->prepare($query);
         $statement->bindValue(':movieIds', $movieId, PDO::PARAM_INT);
 
         if ($statement->execute()) {
             header("Location: index.php");
-        }  
-        
+        } 
+
         $image = $statement->fetch();
     }
 
+    // Sort comments by newest to oldest
     $query = "SELECT * FROM comments 
     WHERE movieIdFk = ".$row['movieId']."
     ORDER BY date DESC";
@@ -66,6 +81,20 @@
     $statement->execute();
 
     $comment = $statement->fetch();
+
+    // Deleting comments
+    if (isset($_POST['delete'])) {
+        $query = "DELETE FROM comments WHERE commentId = :commentId LIMIT 1";
+        $statement = $db->prepare($query);
+        
+        $movieId = filter_input(INPUT_GET, 'commentId', FILTER_SANITIZE_NUMBER_INT);
+        
+        $statement->bindValue('commentId', $movieId, PDO::PARAM_INT);
+        $statement->execute();
+        
+        $movies = $statement->fetch();   
+
+    }
 ?>
 
 
@@ -78,14 +107,20 @@
         <title>Full post</title>
     </head>
     <div id="header">
-            <ul id="nav-buttons">
-                <li><a class="text-decoration-none" href="index.php"><h1>Andy's Movie Blog</h1></a></li>
-                <div class="post-buttons">
-                <li><a id="new-post" href="create.php">New Post</a></li>
-                <li><a id="home-page" href="index.php">Home</a></li>
-                </div>
-            </ul>                         
-        </div> 
+        <ul id="nav-buttons">
+            <li><a class="text-decoration-none" href="index.php"><h1>Andy's Movie Blog</h1></a></li>
+            <div class="post-buttons">
+            <li><a id="new-post" href="create.php">New Post</a></li>
+            <li><a id="home-page" href="index.php">Home</a></li>
+            </div>
+        </ul>
+        
+        <form action="search.php" method="POST">
+            <label>Search</label>
+            <input type="text" name="search">
+            <input type="submit" name="submit">
+        </form>
+    </div> 
     <body>
         <div class="container">
         <a class="text-decoration-none" href="index.php"><h1><?= $row['movieName'] ?></h1></a>
@@ -95,11 +130,11 @@
                 <div>
                     <input type="file" name="image">
                 </div>
-                <input type="submit" name="upload">
+            <input type="submit" value="Upload" name="upload">
         </form>
 
         
-        <div>
+        <div>        
             <img src="images/<?= $row['image'] ?>">
         </div>
 
@@ -137,7 +172,10 @@
                     <h5>Content</h5>
                     <p><?= $comments['content'] ?></p>
                 </div>
-                <small><?= "Posted on: " . date("F j, Y, g:i a", strtotime($comments['date'])) ?></small>
+                <small><?= "Posted on: " . $comments['date'] ?></small>
+                <form method="post">
+                <input type="submit" name="delete" value="delete">
+                </form>
             </div>
         <?php endwhile ?>
     </body>
